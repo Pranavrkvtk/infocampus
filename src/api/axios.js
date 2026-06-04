c1d51a762bf63fceda356bb65947021aff8a5bed
Pranav R k <vtkrkpranav@gmail.com>
2026-06-04 17:28:18 +0530

    new update in axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,25 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backendrender-3-3pdg.onrender.com/api",
+  baseURL: "http://localhost:8080/api",
   headers: {
     "Content-Type": "application/json",
   },
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-
-  // Don't send JWT for login/register
-  if (
-    token &&
-    config.url !== "/auth/login" &&
-    config.url !== "/auth/register"
-  ) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
 });
 
 export default api;
