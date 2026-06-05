51161851940b39cf0fee890b404dd62b30735cb7
Pranav R k <vtkrkpranav@gmail.com>
2026-06-05 08:17:40 +0530

    new updates

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",  // ✅ Change to local backend
+  baseURL: "https://backendrender-3-3pdg.onrender.com/api",  // ✅ Change to local backend
   headers: {
     "Content-Type": "application/json",
   },
