dcfb3ca0c761304388bb80792141994f27e186ed
Pranav R k <vtkrkpranav@gmail.com>
2026-06-05 14:59:33 +0530

    new changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backendrender-3-3pdg.onrender.com/api",  // ✅ Change to local backend
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080/api",
   headers: {
     "Content-Type": "application/json",
   },
