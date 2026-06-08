af182fca908a6fe4546e322776a966a969ee5f13
Pranav R k <vtkrkpranav@gmail.com>
2026-06-08 12:07:02 +0530

    changed

--- src/components/Instructor/InstructorDashboard.jsx
+++ src/components/Instructor/InstructorDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import Swal from "sweetalert2";
 
 // ================= COLORS =================
